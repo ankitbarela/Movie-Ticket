66b12a1c4acf5a18a2b7b8c654137b5075c4c0c5
Amit Vishwakarma <mail.amitvishwakarma@gmail.com>
2023-01-21 19:50:49 +0530

    Add existing project files to Git

---
+++ MovieTicketApi/Db/MovieContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketApi.Db
+{
+    public class MovieContext  :DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+   : base(options)
+        {
+        }
+
+        public DbSet<Model.City> City { get; set; }
+        public DbSet<Model.Movie> Movie { get; set; }
+        public DbSet<Model.State> State { get; set; }
+    }
+}
