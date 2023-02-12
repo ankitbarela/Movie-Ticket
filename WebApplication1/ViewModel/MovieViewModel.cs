584ca2109188e9a44ab463b47815418b2afea086
Ankit Vishwakarma <ankitvish1306@gmail.com>
2023-02-12 09:47:36 +0530

    some changes

--- WebApplication1/ViewModel/MovieViewModel.cs
+++ WebApplication1/ViewModel/MovieViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class MovieViewModel
     {
+        public int MovieId { get; set; }
         public string MovieName { get; set; }
         public string DirectorName { get; set; }
         public string ProducerName { get; set; }
